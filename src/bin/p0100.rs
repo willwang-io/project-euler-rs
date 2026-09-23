62eb3e200185dbd21a388fb087eb7381e166aa13
willwang-io <hello@willwang.io>
2026-09-23 11:18:27 -0600

    Add 100

---
+++ src/bin/p0100.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut b1 = 1u64;
+    let mut b2 = 3u64;
+    let mut r1 = 0u64;
+    let mut r2 = 1u64;
+
+    while b2 + r2 <= 1e12 as u64 {
+        (b1, b2) = (b2, 6 * b2 - b1 - 2);
+        (r1, r2) = (r2, 6 * r2 - r1);
+    }
+
+    println!("{b2}");
+}
